a80f2854f50e11b36ddf44204936337ec3624ce5
Philip Noebauer <pnoebauer>
2021-07-26 08:34:10 +0800

    first commit

---
+++ src/RandomQuoteMachine.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import React from 'react';
+import './RandomQuoteMachine.css';
+
+class RandomQuoteMachine extends React.Component {
+	render() {
+		return (
+			<div
+				className='container-fluid bg-danger'
+				style={{height: '100%', position: 'fixed', top: '0', left: '0'}}
+			>
+				<div className='w-100 h-100'>Test</div>
+			</div>
+		);
+	}
+}
+
+export default RandomQuoteMachine;
